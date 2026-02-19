f58e40d0cf5192b27d3047a7b2ab13fd64b41eca
João Pedro Barros <joaopedrobarrosdesousa23@gmail.com>
2026-02-19 11:39:56 -0300

    Add files via upload

---
+++ Jogo-de-Adivinhação-em-Python.py
@@ -0,0 +1,22 @@
+print("--- Jogo de adivinhação ---\n")
+
+import random
+
+sorteio = random.randint(0,10)
+tentativas = 0
+
+while True:
+    num = int(input("Estou pensando em um número entre 0 e 10, consegue adivinhar qual é ?\nResposta: "))
+    
+    if 0 <= num <= 10 :
+            
+        while num != sorteio:
+            num = int(input("Você errou! Tente novamente.\nNúmero: "))
+            tentativas += 1
+            
+        if num == sorteio:
+            print(f"\nVocê acertou! Eu pensei no número {sorteio} e você precisou de {tentativas} tentaivas!")
+            break
+        
+    else:
+        print("Somente entre 0 e 10! Tente novamente!\n")
